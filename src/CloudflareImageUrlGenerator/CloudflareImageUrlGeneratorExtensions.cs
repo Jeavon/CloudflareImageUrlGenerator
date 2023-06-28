92e11bfdb18e60be1c69f5ea4e5518c04424142c
Jeavon Leopold <jeavon@crumpled-dog.com>
2023-06-28 12:49:23 +0100

    Not need to pass config

--- src/CloudflareImageUrlGenerator/CloudflareImageUrlGeneratorExtensions.cs
+++ src/CloudflareImageUrlGenerator/CloudflareImageUrlGeneratorExtensions.cs
@@ -8,12 +8,12 @@ namespace CloudflareImageUrlGenerator
 {
     public static class CloudflareImageUrlGeneratorExtensions
     {
-        public static IUmbracoBuilder AddCloudflareImageUrlGenerator(this IUmbracoBuilder builder, IConfiguration config)
+        public static IUmbracoBuilder AddCloudflareImageUrlGenerator(this IUmbracoBuilder builder)
         {
             ArgumentNullException.ThrowIfNull(builder);
 
             var cloudflareImageUrlGeneratorOptions = new CloudflareImageUrlGeneratorOptions();
-            var cloudflareImageUrlGeneratorSection = config.GetSection(CloudflareImageUrlGeneratorOptions.CloudflareImageUrlGeneratorSection);
+            var cloudflareImageUrlGeneratorSection = builder.Config.GetSection(CloudflareImageUrlGeneratorOptions.CloudflareImageUrlGeneratorSection);
             cloudflareImageUrlGeneratorSection.Bind(cloudflareImageUrlGeneratorOptions);
             builder.Services.Configure<CloudflareImageUrlGeneratorOptions>(cloudflareImageUrlGeneratorSection);
 
