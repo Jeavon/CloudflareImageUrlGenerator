f68fe530871f6c8a4514731c65093b129db19a62
Jeavon Leopold <jeavon@crumpled-dog.com>
2023-06-28 12:11:03 +0100

    Add option to disable generator

---
+++ src/CloudflareImageUrlGenerator/CloudflareImageUrlGeneratorExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Media;
+using Umbraco.Extensions;
+
+namespace CloudflareImageUrlGenerator
+{
+    public static class CloudflareImageUrlGeneratorExtensions
+    {
+        public static IUmbracoBuilder AddCloudflareImageUrlGenerator(this IUmbracoBuilder builder, IConfiguration config)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+
+            var cloudflareImageUrlGeneratorOptions = new CloudflareImageUrlGeneratorOptions();
+            var cloudflareImageUrlGeneratorSection = config.GetSection(CloudflareImageUrlGeneratorOptions.CloudflareImageUrlGeneratorSection);
+            cloudflareImageUrlGeneratorSection.Bind(cloudflareImageUrlGeneratorOptions);
+            builder.Services.Configure<CloudflareImageUrlGeneratorOptions>(cloudflareImageUrlGeneratorSection);
+
+            if (cloudflareImageUrlGeneratorOptions.Enabled)
+            {
+                builder.Services.AddUnique<IImageUrlGenerator, HybridCloudflareImageSharpImageUrlGenerator>();
+            }
+            return builder;
+        }
+    }
+}
---
+++ src/CloudflareImageUrlGenerator/CloudflareImageUrlGeneratorOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudflareImageUrlGenerator
+{
+    public sealed class CloudflareImageUrlGeneratorOptions
+    {
+        public const string CloudflareImageUrlGeneratorSection = "CloudflareImageUrlGenerator";
+
+        public bool Enabled { get; set; } = true;
+    }
+}
