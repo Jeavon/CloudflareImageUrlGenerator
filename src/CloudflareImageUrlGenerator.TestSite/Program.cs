4def5f631c35f87da4c7e54128cc963779e2c4ea
Jeavon Leopold <jeavon@crumpled-dog.com>
2024-07-03 12:07:23 +0100

    Complete setup of test site

--- src/CloudflareImageUrlGenerator.TestSite/Program.cs
+++ src/CloudflareImageUrlGenerator.TestSite/Program.cs
@@ -1,3 +1,5 @@
+using CloudflareImageUrlGenerator;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.CreateUmbracoBuilder()
@@ -5,6 +7,7 @@ builder.CreateUmbracoBuilder()
     .AddWebsite()
     .AddDeliveryApi()
     .AddComposers()
+    .AddCloudflareImageUrlGenerator()
     .Build();
 
 WebApplication app = builder.Build();
