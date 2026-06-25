c6f0ec0fd01316d60402a619e790fac8d6f71ab1
Jeavon Leopold <jeavon@crumpled-dog.com>
2026-06-25 11:20:46 +0100

    feat: integrate Slimsy into test site for responsive image handling

--- src/CloudflareImageUrlGenerator.TestSite17/Program.cs
+++ src/CloudflareImageUrlGenerator.TestSite17/Program.cs
@@ -1,5 +1,6 @@
 
 using CloudflareImageUrlGenerator;
+using Slimsy.DependencyInjection;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
     .AddComposers()
+    .AddSlimsy()
     .AddCloudflareImageUrlGenerator()
     .Build();
 
